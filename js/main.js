fdedd95fb73c0a89d6869537cb35185f9756806d
Serafima <serafima93.93@gmail.com>
2021-02-01 13:17:23 +0200

    Третьи правки

--- js/main.js
+++ js/main.js
@@ -1,84 +1,34 @@
-/* По мне этот вариант красивее
+// first function
 
 const getRandomIntInclusive = function (min, max) {
-  let result
-  if (min < 0) {
-    return 'Введите число больше 0!';
-  }
-  if (max <= min) {
-    return 'Максимум не может быть меньше минимума!';
-  }
+  let randomNumber
 
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  result = Math.floor(Math.random() * (max - min + 1)) + min;
-
-  return result
+  if (min < 0 || max <= min) {
+    randomNumber = 'Введите число больше 0! Максимум не может быть меньше минимума!'
+  }
+  else {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  return randomNumber
 }
 
-getRandomIntInclusive(0, 10); */
+getRandomIntInclusive(10, 111);
 
 
+// second function
 
-const getRandomIntInclusive = function (min, max) {
-  let result
-  let mistake
-  if (min < 0) {
-    result = min;
-    mistake = result;
-  }
-  if (max <= min) {
-    result = max;
-    mistake = result;
-  }
-  if (mistake) {
-    return 'Введите число больше 0! Максимум не может быть меньше минимума!'
-  }
-
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  result = Math.floor(Math.random() * (max - min + 1)) + min;
-
-  return result
-}
-
-getRandomIntInclusive(0, 10);
-
-
-/*  Этот вариант мне также кажется более красивым
 
 const getRandomArbitrary = function (min, max, n) {
   let randomNumber
 
-  if (min < 0) {
-    return 'Введите число больше 0!';
+  if (min < 0 || max <= min) {
+    randomNumber = 'Введите число больше 0! Максимум не может быть меньше минимума!'
   }
-  if (max <= min) {
-    return 'Максимум не может быть меньше минимума!';
+  else {
+    randomNumber = (Math.random() * (max - min + 1) + min).toFixed(n);
   }
-  randomNumber = Math.random() * (max - min + 1) + min;
-
-  return randomNumber.toFixed(n)
+  return randomNumber
 }
-getRandomArbitrary(0, 10, 2);
-*/
-
-const getRandomArbitrary = function (min, max, n) {
-  let randomNumber
-
-  let mistake
-  if (min < 0) {
-    randomNumber = min;
-    mistake = randomNumber;
-  }
-  if (max <= min) {
-    randomNumber = max;
-    mistake = randomNumber;
-  }
-  if (mistake) {
-    return 'Введите число больше 0! Максимум не может быть меньше минимума!'
-  }
-  randomNumber = Math.random() * (max - min + 1) + min;
-  return randomNumber.toFixed(n)
-}
-getRandomArbitrary(0, 10, 2);
+getRandomArbitrary(9, 10, 2);
